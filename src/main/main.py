2452c9c80006740c752045628e51615efcf3702e
= <=>
2023-01-04 19:44:19 -0600

    Add Main Function Program

---
+++ src/main/main.py
@@ -0,0 +1,25 @@
+import print_option as printer
+
+
+# import util as util
+
+
+def main():
+    """
+    ctrl_while = True
+    while ctrl_while:
+        util.home()
+        option_menu = util.get_option_menu()
+        util.get_values_by_user(option_menu)
+        printer.print_menu_salida()
+        option_out = util.get_option_out()
+        if option_out == "11":
+            util.get_values_by_user(option_menu)
+            printer.print_menu_salida()
+            option_out = util.get_option_out()
+        if option_out == "12":
+            ctrl_while = False
+    """
+
+
+main()
